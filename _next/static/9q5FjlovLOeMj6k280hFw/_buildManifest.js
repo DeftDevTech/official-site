f372f9020d9085ae546d9192a05ebda5455fbdf5
wantipDeftdev <112279932+wantipDeftdev@users.noreply.github.com>
2023-01-09 10:02:21 +0000

    Deploying to gh-pages from @ DeftDevTech/official-site@704c44753af8a599dd09fb932a5fecefd9fc3d1b 🚀

---
+++ _next/static/9q5FjlovLOeMj6k280hFw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b7486709a5b739de.css","static/chunks/197-89af4d44f12e28a3.js","static/chunks/pages/index-a2cf3f2a3a256e08.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
