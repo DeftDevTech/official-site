f5bbb7897e9352099fb30736796f90778858ea6a
yoh-dd <121992104+yoh-dd@users.noreply.github.com>
2023-01-12 17:24:48 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,13 +4,13 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 let assetPrefix = '';
 let basePath = '';
 
-// if (isGithubActions) {
-//   // trim off `<owner>/`
-//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
 
-//   assetPrefix = `/${repo}/`;
-//   basePath = `/${repo}`;
-// }
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
 
 module.exports = {
   basePath,
