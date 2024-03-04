7d2323f6c1b02e0a9d95eff6abfe08040077ec08
yoh-dd <121992104+yoh-dd@users.noreply.github.com>
2024-03-04 03:31:02 +0000

    Deploying to gh-pages from @ DeftDevTech/official-site@56bf4a198bf15febc035e6362f9562bde894f573 🚀

--- _next/static/h-fvs_OhBzwjhIB9BAFtj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5c1eed2607cfa2e9.css","static/chunks/197-89af4d44f12e28a3.js","static/chunks/pages/index-df7a64ad97371fb1.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
